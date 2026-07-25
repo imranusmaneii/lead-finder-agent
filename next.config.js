74f9fd8b94ad0ef9f46ef2e1dc0d123061a10d57
Imran Usmani <iusmani364@gmail.com>
2026-07-26 00:59:36 +0500

    chore: simplify next.config.js and add serverRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
     serverComponentsExternalPackages: ["playwright-core", "@sparticuz/chromium"],
+  },
+  serverRuntimeConfig: {
+    maxDuration: 60,
   },
 };
 
